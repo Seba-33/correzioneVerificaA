e64772238d0406560ee56bab507acf92bd6a886b
Seba-33 <159021649+Seba-33@users.noreply.github.com>
2025-05-22 07:44:30 +0000

    secondo punto

--- correz/src/app/Models/driving.model.ts
+++ correz/src/app/Models/driving.model.ts
@@ -1,3 +1,13 @@
 export class Mezzi {
-    
+    tipo : string
+    descrizione : string
+    tariffa : string
+    valutazionemedia : string
+
+    constructor(tipo: string, descrizione: string, tariffa: string, valuatazionemdeia: string){
+        this.tipo = tipo 
+        this.descrizione = descrizione
+        this.tariffa = tariffa
+        this.valutazionemedia = valuatazionemdeia
+    }
 }
